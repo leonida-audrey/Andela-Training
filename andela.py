4601316e9c324cde6e564ed57af1613779693571
leonida-audrey <linetaudrey@gmail.com>
2018-06-02 11:11:58 +0300

    Second Commit

--- andela.py
+++ andela.py
@@ -1,2 +1,43 @@
 print('today is saturday!')
 print('hello')
+list2 = [1, 2, 3, 4, 5 ];
+print(list2) 
+list3 = ['My name is Audrey']    
+print(list3) 
+list4 = [1,'My name is Audrey']
+print(list4)  
+#this is a list
+
+#beggining of tuple
+tuple1 = tuple('What is your name my dear friend')  
+print(tuple1)
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)                      
+tuple2 = ('what is your name!')
+print(tuple2)
+tup1 = (12, 34.56)
+tup2 = ('abc', 'xyz')
+
+# Following action is not valid for tuples
+# tup1[0] = 100;
+
+# So let's create a new tuple as follows
+tup3 = tup1 + tup2
+print (tup3)
+#end of tuple
+
+#creating my own sets
+thisset = {"Joyce", "Yvonne", "Audrey"}
+print(thisset)
+#collase of set
+
+#dictionary
+thisdict =	{
+  "Name": "Audrey",
+  "Age": "22",
+  "Nationality": "Kenyan",
+  "Gender": "Female"
+}
+print(thisdict)
+
+#collapse of dictionary
